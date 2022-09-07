99c723354cd95b62e9c59f0a573e9af658d7abff
Candelario <candelario.hernandez@induxsoft.net>
2022-09-06 20:02:19 -0500

    se agrego el metodo writeTextUTF8 para linux

---
+++ LinuxFixes/LinuxFixes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace LinuxFixes
+{
+    public static class File
+    {
+        public static void WriteAllTextUTF8(string filename, string content, bool bomMark)
+        {
+
+            if (System.IO.File.Exists(filename))
+                System.IO.File.Delete(filename);
+
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding(bomMark);
+
+            byte[] data = encoder.GetBytes(content);
+
+            using (FileStream fs = new FileStream(
+                filename, FileMode.CreateNew,
+                FileAccess.Write, FileShare.None))
+            {
+                fs.Write(data, 0, data.Length);
+                fs.Close();
+            }
+        }
+    }
+}
